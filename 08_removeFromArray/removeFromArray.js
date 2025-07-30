ca22c6eca0d86f1d41404c90c16e31fa6f0c8eb2
Alberto <albi.web@gmail.com>
2025-07-30 22:49:45 +0200

    fixed mistake exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,5 +2,6 @@ const removeFromArray = function(array, ...removed) {
     // Restituisce un nuovo array che include solo gli elementi
     // che NON sono presenti nell'array 'removed'.
     return array.filter(item => !removed.includes(item));
+}
 // Do not edit below this line
 module.exports = removeFromArray;
