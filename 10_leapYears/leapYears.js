30511ec99ab9cfe25f40daba18eb4cfb154ac80c
Alberto <albi.web@gmail.com>
2025-07-30 23:16:02 +0200

    Final fix ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
 const leapYears = function(year) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    if (year % 4 === 0){
+        if (year % 100 === 0){
+            return year % 400 === 0;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
