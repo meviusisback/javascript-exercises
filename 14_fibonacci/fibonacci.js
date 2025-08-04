792b44fd5bbd72e3a01026b2d146fdbd00c21b6a
Alberto <albi.web@gmail.com>
2025-08-04 11:44:42 +0200

    Exercise 14 completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  let fib = [1, 1];
+  if (num == 0) return 0;
+  if (num < 0) return "OOPS";
+  for (let i = 2; i <= num; i++) {
+    fib.push(fib[i - 1] + fib[i - 2]);
+  }
+  return fib[num - 1];
 };
 
 // Do not edit below this line
