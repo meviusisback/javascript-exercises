ddd6c30a24ea4e85a0dc84400351635d5c287d5a
Alberto <albi.web@gmail.com>
2025-08-04 13:31:41 +0200

    Exercise 16 completed

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  const getAge = (p) =>
+    (p.yearOfDeath || new Date().getFullYear()) - p.yearOfBirth;
 
+  const oldest = arr.reduce((acc, val) =>
+    getAge(acc) < getAge(val) ? val : acc
+  );
+  return oldest;
 };
 
 // Do not edit below this line
