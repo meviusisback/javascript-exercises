8d5ef66f45918fe4830f631dbf97778f902f242a
Alberto <albi.web@gmail.com>
2025-08-04 11:17:39 +0200

    Exercise 13 completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const letters = [...str.toLowerCase()]
+    .filter((char) => /[a-z0-9]/i.test(char))
+    .join("");
+  return letters.split("").reverse().join("") === letters;
 };
 
 // Do not edit below this line
